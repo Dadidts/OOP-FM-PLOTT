9cbdc404bc542c8212526669f60ecffb25452005
Stas Stas <stanislavredka@gmail.com>
2022-06-25 06:18:33 +0900

    Начал писать в файловом менеджере свой недоГУИ. Пока что, только буфер экрана и размер окна. Дальше буду разираться с позицией окна, что бы было по центру экрана.

---
+++ MyFileManager/GUI/BConsole.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFileManager.GUI
+{
+    public class BConsole
+    {
+        public static void ConsoleMax(int width, int height)
+        {
+            Console.SetBufferSize(width, height);
+            Console.SetWindowSize(width, height);
+           
+
+
+        }
+
+       
+
+
+
+
+    }
+}
--- MyFileManager/Program.cs
+++ MyFileManager/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MyFileManager.GUI;
+
+
+//
+int x = 177;
+int y = 46;
+
+int x1 = 78;
+int y1 = 21;
+//
+
+//
+BConsole.ConsoleMax(x, y);
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Console.ReadKey();
