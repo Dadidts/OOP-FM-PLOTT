a823fe2b5155eed108a16d707823aab785bd81ff
Stas Stas <stanislavredka@gmail.com>
2022-07-05 02:32:02 +0900

    Удаление методов SET для полей объекта класса

---
+++ 2_Lesson/CBankOfRussia3/Account3.cs
@@ -0,0 +1,82 @@
+﻿namespace _2_Lesson.CBankOfRussia3;
+
+public class Account3
+{
+    private string Number;
+    private string Client;
+    private decimal Balance;
+    private TypeAccount3 type;
+    public static int SetNumber;
+
+    public string GetNumber()
+    {
+        return Number;
+    }
+    public string GetClient()
+    {
+        return Client;
+    }
+    public decimal GetBalance()
+    {
+        return Balance;
+    }
+    public TypeAccount3 GetTypeAccount()
+    {
+        return type;
+    }
+    public string NewNumber()
+    {
+        Number = GeneratorNumner();
+        return Number;
+
+    }
+    public string GeneratorNumner()
+    {
+        string num = "40817810";
+        Random random = new Random();
+        for (int i = 0; i < 12; i++)
+        {
+            int temp = random.Next(-1, 10);
+            SetNumber = (random.Next(-1, 10));
+            temp = (SetNumber * temp) / 2;
+            if (temp < 0)
+            {
+                temp = 0;
+            }
+            else if (temp > 9)
+            {
+                temp = 1;
+            }
+            num = num + temp.ToString();
+
+        }
+        return num;
+
+    }
+    public void SetTypeAccountDebet(TypeAccount3 typeAccount)
+    {
+
+        type = TypeAccount3.DEBET;
+
+
+    }
+    public void SetTypeAccountCredit(TypeAccount3 typeAccount)
+    {
+
+        type = TypeAccount3.CREDIT;
+
+
+    }
+    public void SetTypeAccountMixed(TypeAccount3 typeAccount)
+    {
+
+        type = TypeAccount3.MIXED;
+
+
+    }
+    public void Print()
+    {
+        Console.WriteLine($"Клиентe: {Client} открыт счет №: {Number} Тип: {type} Баланс: {Balance} ");
+    }
+
+}
---
+++ 2_Lesson/CBankOfRussia3/TypeAccount3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Lesson.CBankOfRussia3
+{
+    public enum TypeAccount3
+    {
+        DEBET = 1,
+        CREDIT,
+        MIXED
+
+    }
+}
